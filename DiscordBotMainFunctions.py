dfd24cacb7e998d4795f97be93cc7121caf13fc6
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-06-03 13:59:47 +0200

    Simplification

--- DiscordBotMainFunctions.py
+++ DiscordBotMainFunctions.py
@@ -4,6 +4,7 @@ Made By: Mads Hermansen
 Github: https://github.com/KarlofKuwait
 Date: 03/06/2019
 """
+
 from datetime import datetime
 import discord
 import random, time
@@ -15,6 +16,10 @@ champs = [x.strip() for x in open("Champlist.txt").readlines()]
 createlog = False
 logfilename = "Chatlog.txt"
 # End Settings
+
+# Messages
+role_error = "Please specify the role, examples are \n- all \n- top \n- jng \n- mid \n- adc \n- sup"
+# End Messages
 
 client = discord.Client()
 
@@ -30,36 +35,40 @@ async def on_message(message):
     # Respond to hehe
     if message.content.startswith("hehe") or message.content.startswith("Hehe"):
         await channel.send("oh you are an amazing person " + str(author))
+
     # Respond to !Help
     if message.content.startswith("!help") or message.content.startswith("!Help"):
         if len(usermessage) > 2:
             await channel.send("Please specify a working command")
         else:
             await channel.send("""Commands \n- Help \n- Champ \n- Info""")
+
     # Respond to !Champ
     if message.content.startswith("!champ") or message.content.startswith("!Champ"):
         if len(usermessage) > 2:
-            await channel.send(
-                "Please specify the role, examples are \n- all \n- top \n- jng \n- mid \n- adc \n- sup")
+            await channel.send(role_error)
         try:
             if usermessage[1] == "all" or usermessage[1] == "All":
                 await channel.send(random.choice(champs))
             else:
-                await channel.send(
-                    "Please specify the role, examples are \n- all \n- top \n- jng \n- mid \n- adc \n- sup")
+                await channel.send(role_error)
         except IndexError:
-            await channel.send(
-                "Please specify the role, examples are \n- all \n- top \n- jng \n- mid \n- adc \n- sup")
+            await channel.send(role_error)
+
     # Respond to !Info need Riot API
     if message.content.startswith("!info") or message.content.startswith("!Info"):
         await channel.send("Currently this command doesnt work, instead try !Champ")
-    # Review the sent message (Will create log?)
+
+    # Review the sent message (Optional log)
     now = datetime.now()
-    dt_string = now.strftime("[%d/%m/%Y:%H/%M/%S]")
+    dt_string = now.strftime("[%d/%m/%Y %H:%M:%S]")
     log = (str(channel) + dt_string + str(author) + " sent \"" + str(message.content) + "\"")
-    if createlog == True:
-        # Create log
-        # Check if file exists and writes
+    # Create log
+    if createlog:
+        """
+        I know discord automatically logs everything and it is saved
+        onto their servers, it is optional if a custom log is created
+        """
         try:
             prevlog = [x.strip() for x in open(logfilename).readlines()]
 
